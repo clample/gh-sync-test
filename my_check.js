5d2a8a47fb14de56900c8bda753a6a21c16bcb6b
Chris Lample <chris@checklyhq.com>
2022-07-27 14:19:26 +0200

    Update my test

--- my_check.js
+++ my_check.js
@@ -1,8 +1,24 @@
-const myLib = require('./my_other_lib2')
+const { chromium } = require('./playwright.js')
 
+// Checkly supports top level await, but we wrap your code in an async function so you can run it locally too.
+async function run () {
+  const browser = await chromium.launch()
+  const page = await browser.newPage()
 
-if (myLib) {
-  console.log('IT STILL WORKS! ', myLib)
-} else {
-  throw new Error('It doesnt work')
+  // We visit the page. This waits for the "load" event by default.
+  const response = await page.goto('https://google.com')
+
+  // If the page doesn't return a successful response code, we fail the check.
+  if (response.status() > 399) {
+    throw new Error(`Failed with response code ${response.status()}`)
+  }
+
+  // We snap a screenshot.
+  await page.screenshot({ path: 'screenshot.jpg' })
+
+  // We close the page to clean up and gather performance metrics.
+  await page.close()
+  await browser.close()
 }
+
+run()
