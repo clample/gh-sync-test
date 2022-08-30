621a7b46710026848feb4e0b811f56d8c9e13ba7
Chris Lample <chris@checklyhq.com>
2022-08-30 09:34:53 +0200

    Break the check

--- test/my_check.js
+++ test/my_check.js
@@ -1,6 +1,6 @@
 const myLib = require('../my_lib')
 
-console.log('MADE CHANGES 3')
+console.log('MADE CHANGES 3')lasdfkfj
 if (myLib) {
   console.log('IT still WORKS - even nested! ', myLib)
 } else {
